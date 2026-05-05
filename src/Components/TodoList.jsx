1aea4a57bd62ae9fafd67eca4079d9f0a835c552
zocalop <jcb2861@gmail.com>
2026-05-05 13:15:29 -0300

    Updated TodoList.jsx in main

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+
+  const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});  
 
   return (
     <>
