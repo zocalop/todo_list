c87c56a6b0ab2d6d9b74e2608d97fbc8ed2173ce
zocalop <jcb2861@gmail.com>
2026-05-05 14:02:16 -0300

    Updated TodoList.jsx in main

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -6,6 +6,12 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [headingInput, setHeadingInput] = useState('');
   const [listInputs, setListInputs] = useState({});  
+  const handleAddTodo = () => {
+    if (headingInput.trim() !== '') {
+      setTodos([...todos, { heading: headingInput, lists: [] }]);
+      setHeadingInput('');
+    }
+  };
 
   return (
     <>
@@ -16,13 +22,21 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}}            
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
-        
+        {todos.map((todo, index) => (
+          <div key={index} className="todo-card">
+            <div className="heading_todo">
+              <h3>{todo.heading}</h3>
+              <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
