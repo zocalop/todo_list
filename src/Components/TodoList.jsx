428a74f76e6e6f82e662e7532f6d70cd4757d4e7
zocalop <jcb2861@gmail.com>
2026-05-05 22:06:30 -0300

    Updated TodoList.jsx in main

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -11,7 +11,17 @@ const TodoList = () => {
       setHeadingInput('');
     }
   };
-  const handleAddList = () => 
+  const handleAddList = (index) => {
+    if (listInputs[index] && listInputs[index].trim() !== '') {
+      const newTodos = [...todos];
+      newTodos[index].lists.push(listInputs[index]);
+      setTodos(newTodos);
+      setListInputs({ ...listInputs, [index]: ''});
+    }
+  };       
+  const handleListInputChange = (index, value) => {
+    setListInputs({ ...listInputs, [index]: value});
+  };
 
   return (
     <>
@@ -35,6 +45,13 @@ const TodoList = () => {
               <h3>{todo.heading}</h3>
               <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
             </div>
+            <ul>
+              {todo.lists.map((list, listIndex) => (
+                <li key={listIndex} className='todo_inside_list'>
+                  <p>{list}</p>
+                </li>
+              ))}
+            </ul>
             <div className="add_list">
               <input
                 type="text"
